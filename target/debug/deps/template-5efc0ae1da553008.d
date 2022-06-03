31dce9dd62817ecf4b740b81f35c0804c36d5916
helium18 <helium.dev@tuta.io>
2022-06-03 08:55:43 +0530

    chore(rm): remove *.lock & result/ & target/

--- target/debug/deps/template-5efc0ae1da553008.d
+++
@@ -1,5 +0,0 @@
-/home/helium/code/rust/template/target/debug/deps/template-5efc0ae1da553008: src/main.rs
-
-/home/helium/code/rust/template/target/debug/deps/template-5efc0ae1da553008.d: src/main.rs
-
-src/main.rs:
--- target/debug/template.d
+++
@@ -1,1 +0,0 @@
-/home/helium/code/rust/template/target/debug/template: /home/helium/code/rust/template/src/main.rs
