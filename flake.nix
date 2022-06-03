d9d230b3b090f78b0fc99a0ff8cf392f03e5dfa1
helium18 <helium.dev@tuta.io>
2022-06-03 13:47:04 +0530

    chore: remove comments, readme & license

--- flake.nix
+++ flake.nix
@@ -11,36 +11,24 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, fenix, ... }:
-    # Output schema from https://nixos.wiki/wiki/Flakes
     flake-utils.lib.eachDefaultSystem (system:
       let 
         pname = "template";
         version = "0.1.0";
-        pkgs = nixpkgs.legacyPackages.${system}; # legacyPackages is a workaround for using packages from ol' nixpkgs
+        pkgs = nixpkgs.legacyPackages.${system}; 
       in
       {
-        # called when `nix build` / `nix run` is invoked
         defaultPackage = (pkgs.makeRustPlatform {
-
-          # use nightly rustc and cargo provided by fenix to run/build stuff 
-          # options: minimal | complete
           inherit (fenix.packages.${system}.minimal) cargo rustc;
         }).buildRustPackage {
           pname = pname;
           version = version;
           src = ./.;
 
-          # cargohash isn't taken: https://github.com/nix-community/fenix/issues/70#issuecomment-1114333311
           cargoLock.lockFile = ./Cargo.lock; 
-          
-          # for other makeRustPlatform features see: 
-          # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
         };
 
-        # called when `nix develop` is invoked
         devShells.default = pkgs.mkShell {
-
-          # use nightly cargo & rustc provided by fenix. Add for packages for the dev shell here
           buildInputs = with fenix.packages.${system}.minimal; [ cargo rustc ]; 
         };
       }
